8f364f1106c78c170cc1a716fd1aae1c00d5f15c
sasaki takeru <sasaki.takeru@gmail.com>
2014-05-06 03:44:03 +0900

    reset cocos2d state

--- app_root/boot.rb
+++ app_root/boot.rb
@@ -1,7 +1,10 @@
 begin
   Cocos2dx::CCFileUtils.sharedFileUtils.removeAllPaths
+  Cocos2dx::CCFileUtils.sharedFileUtils.purgeCachedEntries
+  Cocos2dx::CCTextureCache.sharedTextureCache.removeAllTextures
   Cocos2dxMrubyPlayer.load("$DB/boot.rb")
 rescue => e
   puts "Failed fo load $DB/boot.rb e=#{e.inspect}"
   Cocos2dxMrubyPlayer.load("$APP/demo/menu.rb")
 end
+
