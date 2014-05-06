3e59656b95d894a989b5ef09e0f0236472b7644f
sasaki takeru <sasaki.takeru@gmail.com>
2014-05-06 20:03:03 +0900

    Add bird sprite on layer.

---
+++ app_root/demo/flappy/bird.rb
@@ -0,0 +1,6 @@
+class Bird < Sprite
+  def initialize
+    @cc_class_name = 'CCSprite'
+    super("images/bird.png")
+  end
+end
--- app_root/demo/flappy/flappy.rb
+++ app_root/demo/flappy/flappy.rb
@@ -1,15 +1,27 @@
 include Cocos2dx
 fu = CCFileUtils.sharedFileUtils
 fu.addSearchPath(fu.fullPathFromRelativeFile("../..", __FILE__))
+fu.addSearchPath(fu.fullPathFromRelativeFile(".",     __FILE__))
 Cocos2dxMrubyPlayer.load("lib/cocos2dx_support.rb")
 Cocos2dx::Logger.add(Cocos2dx::WebSocketLogger.new("ws://192.168.0.6:9292"))
 log "SearchPaths: #{fu.getSearchPaths.inspect}"
 
+Cocos2dxMrubyPlayer.load("bird.rb")
 class FlappyApp
   attr_reader :scene
   def initialize
     log "FlappyApp#initialize"
+    @win_size = CCDirector.sharedDirector.getWinSize
+    log @win_size
+
+    @bird  = Bird.new
+    @bird.setPosition(@win_size.width/2, @win_size.height/2)
+
+    @layer = Layer.new
+    @layer.addChild(@bird)
+
     @scene = Scene.new
+    @scene.addChild(@layer)
   end
 end
 
