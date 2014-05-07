5897edecff6619c3ec920c885ae6fae745c7d2ab
sasaki takeru <sasaki.takeru@gmail.com>
2014-05-07 13:39:19 +0900

    random wall height

--- app_root/demo/flappy/wall.rb
+++ app_root/demo/flappy/wall.rb
@@ -5,23 +5,36 @@ class Wall < DrawNode
     super
 
     @vx     = -200
-    @width  =   50
-    @height =  200
-    _draw
+    @x = FlappyApp.width
+    @y = FlappyApp.height/2
 
-    setPosition(FlappyApp.width, FlappyApp.height/2)
+    [-1, 1].each do |z|
+      dy = z * (320+150)
+      _draw_rect(
+        0, 0+dy,
+        100, 640,
+        0.0, 0.8, 0.0, 1.0,
+        3,
+        0.0, 0.6, 0.0, 1.0
+      )
+      _draw_rect(
+        0, 0+dy-z*320,
+        130, 40,
+        0.0, 0.8, 0.0, 1.0,
+        3,
+        0.0, 0.6, 0.0, 1.0
+      )
+    end
+    update(0)
   end
 
-  def _draw
+  def _draw_rect(x, y, w, h, r0, g0, b0, a0, border, r1, g1, b1, a1)
     points = [
-      ccp(-@width/2, -@height/2),
-      ccp( @width/2, -@height/2),
-      ccp( @width/2,  @height/2),
-      ccp(-@width/2,  @height/2)
+      ccp(-w/2+x, -h/2+y),
+      ccp( w/2+x, -h/2+y),
+      ccp( w/2+x,  h/2+y),
+      ccp(-w/2+x,  h/2+y)
     ]
-    r0, g0, b0, a0 = 0.0, 0.8, 0.0, 1.0
-    r1, g1, b1, a1 = 0.0, 0.6, 0.0, 1.0
-    border = 2
     drawPolygon(points,
       ccc4f(r0, g0, b0, a0),
       border,
@@ -30,12 +43,12 @@ class Wall < DrawNode
   end
 
   def update(dt)
-    pos = getPosition
-    pos.x += @vx * dt
-    if pos.x < 0
-      pos.x = FlappyApp.width
+    @x += @vx * dt
+    if @x < 0
+      @x = FlappyApp.width
+      @y = (rand*2-1) * 150 + FlappyApp.height/2
     end
-    setPosition(pos)
+    setPosition(@x,@y)
   end
 end
 rescue => e
